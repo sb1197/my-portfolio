384a3dec3bd1299667ccfef38046d9db82ba8e9f
sb1197 <shwetabochare11@gmail.com>
2026-08-05 13:43:18 +0530

    feat: enhance ProjectsSection interactivity and update avatar styles

--- src/components/DetailsSection.jsx
+++ src/components/DetailsSection.jsx
@@ -4,19 +4,19 @@ function DetailsSection({ education, languages, hobbies }) {
   return (
     <>
       <SectionShell title="" className="details-grid">
-      <article>
-        <h3>Education</h3>
-        <ul className="detail-list">
-          {education.map((item) => (
-            <li key={`${item.degree}-${item.institution}`}>
-              <strong>{item.degree}</strong>
-              <span>{item.institution}</span>
-              <span>{item.period}</span>
-              {item.details ? <span>{item.details}</span> : null}
-            </li>
-          ))}
-        </ul>
-      </article>
+        <article>
+          <h3>Education</h3>
+          <ul className="detail-list">
+            {education.map((item) => (
+              <li key={`${item.degree}-${item.institution}`}>
+                <strong>{item.degree}</strong>
+                <span>{item.institution}</span>
+                <span>{item.period}</span>
+                {item.details ? <span>{item.details}</span> : null}
+              </li>
+            ))}
+          </ul>
+        </article>
       </SectionShell>
       <SectionShell title="" className="details-grid">
         <article>
