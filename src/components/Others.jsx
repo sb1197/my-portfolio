d54887bbf21ac733d9efe8d631160944c7515ada
sb1197 <shwetabochare11@gmail.com>
2026-08-18 14:03:17 +0530

    feat: improve code formatting and consistency across components and styles

--- src/components/Others.jsx
+++ src/components/Others.jsx
@@ -2,30 +2,30 @@ import SectionShell from './SectionShell';
 import '../styles/DetailsSection.css';
 
 function Others() {
-    return (
-        <SectionShell title="" className="details-grid">
-        <article>
-          <h3>Languages</h3>
-          <ul className="detail-list">
-            {languages.map((language) => (
-              <li key={language}>
-                <span>{language}</span>
-              </li>
-            ))}
-          </ul>
-        </article>
-        <article>
-          <h3>Hobbies</h3>
-          <ul className="detail-list">
-            {hobbies.map((hobby) => (
-              <li key={hobby}>
-                <span>{hobby}</span>
-              </li>
-            ))}
-          </ul>
-        </article>
-      </SectionShell>
-    )
+  return (
+    <SectionShell title="" className="details-grid">
+      <article>
+        <h3>Languages</h3>
+        <ul className="detail-list">
+          {languages.map((language) => (
+            <li key={language}>
+              <span>{language}</span>
+            </li>
+          ))}
+        </ul>
+      </article>
+      <article>
+        <h3>Hobbies</h3>
+        <ul className="detail-list">
+          {hobbies.map((hobby) => (
+            <li key={hobby}>
+              <span>{hobby}</span>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </SectionShell>
+  );
 }
 
 export default Others;
